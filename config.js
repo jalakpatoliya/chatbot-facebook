02446b17909c4b3c1cba35895688d3cb03a310ff
jalakpatoliya <30546867+jalakpatoliya@users.noreply.github.com>
2018-08-17 19:15:35 +0530

    config changed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     FB_PAGE_TOKEN: 'EAAfFkxJMuQMBAJG12gEKbb3kf9j0uTzZBi60o4ckMDooAU7djm5KCjfPwDAfusc7j6ZCUSBThXfiAQbHjSYTxNKb0tuNIBHCIzyUZAHzVMftJ7Tbn7sjSZB2tduhtJZCWtyqwZBg9VNtL6ViwVXTGZB7ZCAZA4aZColGHBJPLZBosNY7QZDZD',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
+    FB_VERIFY_TOKEN: 'verifytoken937',
+    API_AI_CLIENT_ACCESS_TOKEN: 'c33a16ebf58c4397a657b18adc3fd532',
     FB_APP_SECRET: '',
-    SERVER_URL: "",
+    SERVER_URL: "https://salty-hollows-.herokuapp.com/",
 };

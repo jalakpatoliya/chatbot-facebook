3b3552ecb6b9c53146d8ae848136512b34c74001
jalakpatoliya <30546867+jalakpatoliya@users.noreply.github.com>
2018-09-10 16:39:07 +0530

    test1

--- app.js
+++ app.js
@@ -8,8 +8,18 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 const uuid = require('uuid');
+const nodemailer = require("nodemailer");
 
-
+var transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+						type: 'OAuth2',
+						user: 'fygecmodasa@gmail.com',
+						clientId: '366431355006-5ut34qv0rodupqs8c4qcadge11154522.apps.googleusercontent.com',
+						clientSecret: 'cVuwdxCKN-ZtMhZ037ePd6lZ',
+						refreshToken: '1/i1zddoS6vhuOPHao1yoKcCKnMQW6UlxfF5yySwfYtBA'
+		}
+})
 // Messenger API parameters
 if (!config.FB_PAGE_TOKEN) {
 	throw new Error('missing FB_PAGE_TOKEN');
@@ -204,7 +214,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 								'.<br> Previous job position: ' + previous_job + '.' +
 								'.<br> Years of experience: ' + years_of_experience + '.' +
 								'.<br> Phone number: ' + phone_number + '.' ;
-								// sendEmail('New job application', emailContent);
+								 sendEmail('New job application', emailContent);
 							}
 						}
 						sendTextMessage(sender,responseText);
@@ -918,6 +928,19 @@ function isDefined(obj) {
 	return obj != null;
 }
 
+function sendEmail(sub,content) {
+	var mailOptions = {
+			from: 'GEC Modasa <fygecmodasa@gmail.com>',
+			to: "jlkplt39@gmial.com",
+			subject: sub,
+			text: content
+	};
+	transporter.sendMail(mailOptions, function(error, info) {
+		if(error) {
+			console.log(error)
+		} 
+	});
+}
 // Spin up the server
 app.listen(port, function () {
 	 console.log('running on port'+ port)
